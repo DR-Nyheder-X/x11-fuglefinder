496a4e21237589154a69958b58ecba663470da21
Jacques Mourier <jacquesmourier@gmail.com>
2016-04-05 16:03:03 +0200

    SectionLink basic styling
    Former-commit-id: 34e6debccd9b6a18cd078da6b9eea4692548140d [formerly 5a148ddeea9bc83dac9b4456edcd268822de302b]
    Former-commit-id: 3d266d3ea0a549d11c5c8f7d309bfc5b629342f4

--- web/static/js/components/HabitatList.js
+++ web/static/js/components/HabitatList.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import habitats from '../lib/habitats'
 import SectionLink from './SectionLink'
+import './HabitatList.css'
 
 export default function HabitatList () {
   return <div className='HabitatList'>
-    {Object.keys(habitats).map((key) => (
-      <SectionLink
-        key={key}
-        to={`/birds/${habitats[key]}`}
-        children={key}
-      />
-    ))}
+    <div className='HabitatList-guide'>
+      Hvor er du?
+    </div>
+    <div className='HabitatList-items'>
+      {Object.keys(habitats).map((key) => (
+        <SectionLink
+          key={key}
+          to={`/birds/${habitats[key]}`}
+          children={key}
+        />
+      ))}
+    </div>
   </div>
 }
-
