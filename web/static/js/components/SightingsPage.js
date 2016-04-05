c6078ceea06a367b4c7afac1381d994f2a738b95
Mikkel Malmberg <mikkel@brnbw.com>
2016-04-05 12:39:16 +0200

    Obey The Standard (tm)
    Former-commit-id: cc237d18322d716c2325945e562901250c8eb8e9 [formerly 3568a0b022dad4b60228258239b2149877776b19]
    Former-commit-id: b323edfb86bb79309c2bb72ca1e74558b5e85e32

--- web/static/js/components/SightingsPage.js
+++ web/static/js/components/SightingsPage.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 import Header from './App/Header'
 
 export default class SightingsPage extends Component {
