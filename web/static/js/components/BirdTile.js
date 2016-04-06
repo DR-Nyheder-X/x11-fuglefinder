625239f0f534f5c5fbf6ac2bbcb97273968a391a
Jacques Mourier <jacquesmourier@gmail.com>
2016-04-06 14:48:27 +0200

    BirdTile and Rating (with mockup styles)

--- web/static/js/components/BirdTile.js
+++ web/static/js/components/BirdTile.js
@@ -1,11 +1,26 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
+import Rating from './App/Rating'
+import './BirdTile.css'
 
 export default function BirdTile ({ bird, habitat }) {
   return <div className='BirdTile'>
-    <Link to={`/habitats/${habitat}/${bird.id}`}>
-      {bird.name}
-    </Link>
+    <div className='BirdTile-inner'>
+      <header className='BirdTile-header'>
+        <Link to={`/habitats/${habitat}/${bird.id}`}>
+          {bird.name}
+        </Link>
+        <button className='BirdTile-found'>F/NF</button>
+      </header>
+      <div className='BirdTile-image'>
+        <Link to={`/habitats/${habitat}/${bird.id}`}>
+          <img src='http://thunderfluff.com/fuglefinder/bird.jpg' />
+        </Link>
+      </div>
+      <div className='BirdTile-rating'>
+        <Rating rating='1' />
+      </div>
+    </div>
   </div>
 }
 
