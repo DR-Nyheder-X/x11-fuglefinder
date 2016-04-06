1027cde9aa53bf0a856c629dcf68758eb10c8d6c
Jacques Mourier <jacquesmourier@gmail.com>
2016-04-06 10:37:36 +0200

    Adding a call-to-action on "Mine fund" (for when the list is empty)
    Former-commit-id: 13822cdc6139e17e65b1551ecf97303e50bafe32 [formerly 696e344d020cce7bc86edd08040deedf50e7b1f0]
    Former-commit-id: 559340e389c28e7b08eb14cf2d58e0e9444211ac

--- web/static/js/components/SightingsPage.js
+++ web/static/js/components/SightingsPage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Header from './App/Header'
+import FullPageNotice from './App/FullPageNotice'
 
 export default class SightingsPage extends Component {
   render () {
     return <div>
       <Header title='Mine fund' />
+      <FullPageNotice to='/' buttonText='Find din første fugl' text='Du har ikke spottet nogen fugle endnu. På med kikkerten.' />
     </div>
   }
 }
