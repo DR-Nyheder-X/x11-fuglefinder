9ddefe6ccde1e515f39a76c0a54d3e50c3631273
Mikkel Malmberg <mikkel@brnbw.com>
2016-04-07 13:12:13 +0200

    Add found prop to BirdTile

--- web/static/js/components/KitchensinkPage.js
+++ web/static/js/components/KitchensinkPage.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 import Header from './App/Header'
+import BirdTile from './BirdTile'
+
+const sampleBird = {
+  name: "Johnfugl"
+}
 
 export default class KitchensinkPage extends Component {
   render () {
     return <div>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+
       <Header />
       <Header title='Ellen Page' />
       <Header title='Jimmy Page' backButton />
       <img style={{width: '100%'}} src='http://s3.amazonaws.com/wolty/media/56/original/Kitchen_Sink.jpeg?1305542976' />
+      <BirdTile bird={sampleBird} habitat={'coast'} found />
+      <BirdTile bird={sampleBird} habitat={'coast'} />
+
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
     </div>
   }
 }
