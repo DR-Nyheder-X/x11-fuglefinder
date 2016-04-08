46ed733b13344c3a2fbc100fd9d7ad4fd78670d7
Mikkel Malmberg <mikkel@brnbw.com>
2016-04-08 14:52:53 +0200

    Add picture uploads

--- web/models/bird.ex
+++ web/models/bird.ex
@@ -1,5 +1,7 @@
 defmodule Birdie.Bird do
   use Birdie.Web, :model
+  alias Birdie.{Repo, Habitat}
+  use Arc.Ecto.Model
 
   schema "birds" do
     field :name, :string
@@ -8,14 +10,18 @@ defmodule Birdie.Bird do
     field :wikipedia_url, :string
     field :rarity, :integer
     field :size, :string
+    field :picture, Birdie.Picture.Type
+    field :habitat_ids, {:array, :integer}, virtual: true
 
-    many_to_many :habitats, Birdie.Habitat, join_through: "birds_habitats", on_delete: :delete_all
+    many_to_many :habitats, Birdie.Habitat,
+      join_through: "birds_habitats", on_delete: :delete_all
 
     timestamps
   end
 
-  @required_fields ~w(name latin_name wikipedia_url rarity size)a
-  @optional_fields ~w(fact)a
+  @required_fields ~w(name latin_name wikipedia_url rarity size fact)a
+  @optional_fields ~w(habitat_ids)a
+  @allowed_upload_fields ~w(picture)a
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -27,5 +33,17 @@ defmodule Birdie.Bird do
     model
     |> cast(params, @required_fields ++ @optional_fields)
     |> validate_required(@required_fields)
+    |> cast_attachments(params, @allowed_upload_fields)
+    |> assoc_habitats
+  end
+
+  defp assoc_habitats set do
+    if Map.has_key?(set.changes, :habitat_ids) do
+      put_assoc(set, :habitats, Enum.map(set.changes.habitat_ids, fn id ->
+        Habitat.changeset Repo.get!(Habitat, id), %{}
+      end))
+    else
+      set
+    end
   end
 end
