e1bb154dc63b5ab35ce1dfde08c2f140d0adecfa
Mikkel Malmberg <mikkel@brnbw.com>
2016-04-09 20:02:03 +0200

    SightingsPage setup
    Former-commit-id: 067389b1c032a11d8905fd4776db072d320cca0b [formerly 310faeac10467a2d8f9613e4e2a20f19b6ebae94]
    Former-commit-id: cb96a205c4350c952567728b41aad1e379aa5acb

--- web/static/js/components/SightingsPage.js
+++ web/static/js/components/SightingsPage.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Header from './App/Header'
 import FullPageNotice from './App/FullPageNotice'
+import Api from '../lib/Api'
+import { register } from '../store'
 
-export default class SightingsPage extends Component {
+/* ACTIONS */
+
+export const CREATE_USER_SIGHTING = 'sightings/CREATE_USER_SIGHTING'
+export function createUserSighting (bird) {
+  return {
+    type: CREATE_USER_SIGHTING,
+    payload: { promise: Api.post('/sightings', { bird_id: bird.id }) }
+  }
+}
+
+/* REDUCER */
+
+export const initialState = {
+  sightings: []
+}
+
+export function reducer (state = initialState, action) {
+  switch (action.type) {
+    default: return state
+  }
+}
+
+register({ sightings: reducer })
+
+/* COMPONENT */
+
+const stateToProps = (state) => ({
+  sightings: state.sightings.sightings
+})
+
+class SightingsPage extends Component {
   render () {
+    const { sightings } = this.props
+
     return <div>
       <Header title='Mine fund' />
-      <FullPageNotice to='/' buttonText='Find din første fugl' text='Du har ikke spottet nogen fugle endnu. På med kikkerten.' />
+      {sightings.length === 0 && (
+        <FullPageNotice to='/' buttonText='Find din første fugl' text='Du har ikke spottet nogen fugle endnu. På med kikkerten.' />
+      )}
     </div>
   }
 }
+
+export default connect(stateToProps)(SightingsPage)
