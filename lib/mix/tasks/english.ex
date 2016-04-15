c31b211f598db1891a9e0ae07c5895741a2c8eea
Mikkel Malmberg <mikkel@brnbw.com>
2016-04-15 11:20:26 +0200

    English import
    Former-commit-id: 67b0d7afe5d2acbf3a6bbc1278973aa16c1ae4ed [formerly a23070611214f5e41214c786cb63390797633d35]
    Former-commit-id: 101b2b8ab7b1dfeb0be2e56acdce14f463330d05

--- lib/mix/tasks/english.ex
+++ lib/mix/tasks/english.ex
@@ -1,6 +1,7 @@
 defmodule Mix.Tasks.Birdie.English do
   use Mix.Task
   alias Birdie.{Repo, Bird, Habitat}
+  import Ecto.Query, only: [from: 2]
 
   @repo "https://docs.google.com/spreadsheets/d/1O5ip1kUvPuGNwfe422DuuAGC-75AEuiLkqSHM_pk8E0/pub?output=csv"
 
@@ -14,11 +15,11 @@ defmodule Mix.Tasks.Birdie.English do
   defp update_english_name line do
     [ _, _name, _latin_name, english, wikipedia_url ] = Enum.take(line, 5)
 
-    case Repo.get_by(Bird, wikipedia_url: wikipedia_url) do
-      %Bird{} = bird ->
-        Bird.changeset(bird, %{english_name: english}) |> Repo.update!
-      _ -> nil
-    end
+    query = from(b in Bird, where: [wikipedia_url: ^wikipedia_url])
+
+    Enum.each(Repo.all(query), fn bird ->
+      Bird.changeset(bird, %{english_name: english}) |> Repo.update!
+    end)
   end
 
   defp fetch_repo do
