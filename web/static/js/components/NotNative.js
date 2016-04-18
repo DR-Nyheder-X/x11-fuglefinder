21de1d8f9bb88e334dae764006ee6de899e070c3
Mikkel Malmberg <mikkel@brnbw.com>
2016-04-18 22:07:47 +0200

    More NotNative
    Former-commit-id: 71a5aa58496c4b28b4a441870bf38e4c4f2548bc [formerly 77dce3c9f9f1b5cda078ed58ea6142482e204b87]
    Former-commit-id: 9910c895d3bfb839658b1ecd807579bcca5302f3

--- web/static/js/components/NotNative.js
+++ web/static/js/components/NotNative.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react'
 
+var isMobile = {
+  Windows: function () {
+    return /IEMobile/i.test(navigator.userAgent)
+  },
+  Android: function () {
+    return /Android/i.test(navigator.userAgent)
+  },
+  BlackBerry: function () {
+    return /BlackBerry/i.test(navigator.userAgent)
+  },
+  iOS: function () {
+    return /iPhone|iPad|iPod/i.test(navigator.userAgent)
+  },
+  any: function () {
+    return (isMobile.Android() || isMobile.BlackBerry() ||
+            isMobile.iOS() || isMobile.Windows())
+  }
+}
+
+const notice = {
+  position: 'fixed',
+  bottom: '10px',
+  left: '25%',
+  right: '25%',
+  padding: '15px',
+  background: 'linear-gradient(lightblue, green)',
+  color: 'yellow',
+  textAlign: 'center',
+  borderRadius: '25px',
+  zIndex: 1000
+}
+
 export default class NotNative extends Component {
   render () {
-    return <div>
-      <h1>Tryk på Del og så på Tilføj til hjemmeskærm for at installere appen</h1>
+    return <div style={{padding: '10px'}}>
+      <img src='/apple-touch-icon.png' style={{width: '100%'}} />
+      <div style={notice}>
+        Klik her og på <strong>Tilføj til hjemmeskærm</strong>
+        <br />
+        ||<br />
+        ▼
+      </div>
     </div>
   }
 }
