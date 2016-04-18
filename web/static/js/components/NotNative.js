32e08aa70b3970d72fa0c3b37ef94355bf40b422
Mikkel Malmberg <mikkel@brnbw.com>
2016-04-18 22:10:04 +0200

    Add Force Native button
    Former-commit-id: 80d861253cf57be281a03d6a50c1503c2dd70d76 [formerly 582a37ba164174c09c0f4cc872dea04e630cfec2]
    Former-commit-id: 1579693569d01a60079d58c3d2ec6ed0a5974492

--- web/static/js/components/NotNative.js
+++ web/static/js/components/NotNative.js
@@ -32,10 +32,17 @@ const notice = {
   zIndex: 1000
 }
 
+const forceNative = (event) => {
+  window.localStorage.setItem('_native', 1)
+  window.location = '/'
+}
+
 export default class NotNative extends Component {
   render () {
     return <div style={{padding: '10px'}}>
       <img src='/apple-touch-icon.png' style={{width: '100%'}} />
+      <button onClick={forceNative}>Bare giv mig app'en</button>
+
       <div style={notice}>
         Klik her og på <strong>Tilføj til hjemmeskærm</strong>
         <br />
