1c12eee82685d6746e0aeac408cd1c0050c9a7e2
Jacques Mourier <jacquesmourier@gmail.com>
2016-04-25 11:49:01 +0200

    Add to homescreen for iOS
    Former-commit-id: 04f1447245089603194944d94f5a2d29e2b0cc58 [formerly 77d8680befe99d4781f46f1f8e888011582870de]
    Former-commit-id: 9fac1800f654d061e3607f7fc9c5b3ceafdd1f2c

--- web/static/js/components/NotNative.js
+++ web/static/js/components/NotNative.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Navigation, { Header } from './Navigation'
+import './NotNative.css'
 
 var isMobile = {
   Windows: function () {
@@ -19,35 +21,34 @@ var isMobile = {
   }
 }
 
-const notice = {
-  position: 'fixed',
-  bottom: '10px',
-  left: '25%',
-  right: '25%',
-  padding: '15px',
-  background: 'linear-gradient(lightblue, green)',
-  color: 'yellow',
-  textAlign: 'center',
-  borderRadius: '25px',
-  zIndex: 1000
-}
-
 const forceNative = (event) => {
   window.localStorage.setItem('_native', 1)
   window.location = '/'
 }
 
 export default class NotNative extends Component {
-  render () {
-    return <div style={{padding: '10px'}}>
-      <img src='/apple-touch-icon.png' style={{width: '100%'}} />
-      <button onClick={forceNative}>Bare giv mig app'en</button>
 
-      <div style={notice}>
-        Klik her og på <strong>Tilføj til hjemmeskærm</strong>
-        <br />
-        ||<br />
-        ▼
+  render () {
+    return <div className='NotNative'>
+      <Header />
+      <button className='NotNative-noThanks' onClick={forceNative}>
+        Bare giv mig app'en
+        <svg width="30px" height="29px" viewBox="0 0 30 29">
+          <g transform="translate(-325.000000, -74.000000)">
+            <g transform="translate(325.000000, 74.000000)">
+              <rect id="Rectangle-1642" fill="#F0E7CE" x="0" y="0" width="30" height="29" rx="2"></rect>
+              <path d="M13.1213203,14.2426407 L9.93933983,11.0606602 C9.35355339,10.4748737 9.35355339,9.52512627 9.93933983,8.93933983 C10.5251263,8.35355339 11.4748737,8.35355339 12.0606602,8.93933983 L15.2426407,12.1213203 L18.4246212,8.93933983 C19.0104076,8.35355339 19.9601551,8.35355339 20.5459415,8.93933983 C21.131728,9.52512627 21.131728,10.4748737 20.5459415,11.0606602 L17.363961,14.2426407 L20.5459415,17.4246212 C21.131728,18.0104076 21.131728,18.9601551 20.5459415,19.5459415 C19.9601551,20.131728 19.0104076,20.131728 18.4246212,19.5459415 L15.2426407,16.363961 L12.0606602,19.5459415 C11.4748737,20.131728 10.5251263,20.131728 9.93933983,19.5459415 C9.35355339,18.9601551 9.35355339,18.0104076 9.93933983,17.4246212 L13.1213203,14.2426407 Z" id="Combined-Shape" fill="#A19D8C"></path>
+            </g>
+          </g>
+        </svg>
+      </button>
+      <div className='NotNative-addToHomescreen'>
+        <p>Tryk på <em></em> og tilføj <strong>Fuglefinder</strong> til din hjemmeskærm</p>
+        <svg width='29px' height='12px' viewBox='0 0 29 12'>
+          <g transform='translate(-173.000000, -560.000000)' fill='#304461'>
+            <path d='M173,560 L202,560 L189.038026,570.727151 C188.188598,571.430126 186.81039,571.429288 185.961974,570.727151 L173,560 Z'></path>
+          </g>
+        </svg>
       </div>
     </div>
   }
